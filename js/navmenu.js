98e639b698b412eeecd6fd10571124798016e034
Steve Schmitt <steves@ideablade.com>
2015-05-03 22:53:33 -0700

    Sample documentation pages

---
+++ js/navmenu.js
@@ -0,0 +1,22 @@
+﻿
+$(window).on('hashchange', function() {
+    var url = location.hash.substring(1);
+    $("li.active").removeClass("active");
+    var li = $('a[href="' + url + '"]').parent();
+    li.addClass("active");
+
+    $("#docpage").load(url);
+});
+
+
+$("a:not('.dropdown-toggle')").click(function(event) {
+    var url = event.target.pathname;
+    if (url == location.pathname) return;
+    if (url && !url.indexOf("#") == 0) {
+        url = "#" + url;
+    }
+    if (url && url != location.hash) {
+        location.hash = url;
+    }
+    return false;
+});
