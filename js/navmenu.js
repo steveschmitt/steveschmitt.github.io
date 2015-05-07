d039de5f5a68d0eae35660b02e92417717749951
Steve Schmitt <steves@ideablade.com>
2015-05-07 12:48:41 -0700

    Separated into subdirectories

--- js/navmenu.js
+++ js/navmenu.js
@@ -1,15 +1,16 @@
-﻿
+
 $(window).on('hashchange', function() {
     var url = location.hash.substring(1);
     $("li.active").removeClass("active");
     var li = $('a[href="' + url + '"]').parent();
     li.addClass("active");
 
+    // load the page content
     $("#docpage").load(url);
 });
 
 
-$("a:not('.dropdown-toggle')").click(function(event) {
+$("a:not('.dropdown-toggle, .navbar-brand')").click(function(event) {
     var url = event.target.pathname;
     if (url == location.pathname) return false;
     if (url && !url.indexOf("#") == 0) {
